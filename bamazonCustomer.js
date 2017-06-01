81f76419459d33846e02f67f5b6f7d565d4dbebd
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-01 18:02:12 -0500

    Added interactivity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var prompt = require('prompt');
 
-// ** HOW TO CONNECT TO A DATABASE IN JS / NODE **
+// ** CONNECTS TO THE DATABASE ** 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,37 +13,55 @@ var connection = mysql.createConnection({
     database: "Bamazon"
 });
 
-// How to call this function and test for an error
+// Calls this function and tests for errors
 connection.connect(function(err) {
     // If there is an error, log it 
     if (err) throw err;
     console.log("connected as id" + connection.threadId);
 });
 
-// How to print the database to the console & log errors
-connection.query('SELECT * FROM Bamazon.products', function(err, response) {
-    if (err) throw err;
-    console.log("===================================================")
-    console.log("               Bamazon Inventory:                  ")
-    console.log("---------------------------------------------------")
-    console.log(response);
-    console.log("===================================================")
+function startStore() {
 
-});
+    // Prints the database to the console & logs errors
+    connection.query('SELECT * FROM Bamazon.products', function(err, response) {
+        if (err) throw err;
+        console.log("===================================================")
+        console.log("               Bamazon Inventory:                  ")
+        console.log("---------------------------------------------------")
+        console.log(response);
+        console.log("===================================================")
 
-// Inqueries on buying w/ ID
-var start = function() {
-    inquirer.prompt({
-        name: "buyItem",
-        type: "input",
-        message: "What is the ID of the item you would like to buy?",
-        // choices: ["POST", "BID"]
-    }).then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid.toUpperCase() === "POST") {
-            postAuction();
-        } else {
-            bidAuction();
+    });
+
+    // Inqueries on buying w/ ID
+    inquirer.prompt([{
+            name: "idRequest",
+            type: "input",
+            message: "What is the ID of the item you would like to buy?"
+        },
+        {
+            name: "quantityRequest",
+            type: "input",
+            message: "How many of this item would you like to buy?"
         }
+    ]).then(function(order) {
+        var quantityRequest = order.quantityRequest;
+        connection.query('SELECT * FROM Bamazon.products WHERE item_id=' + idRequest, function(err, selectedItem) {
+            if (err) throw err;
+            if (selectedItem[0].quantity - quantityRequest >= 0) {
+                console.log("Amount in stock: " + selectedItem[0].stock_quantity + "Order Quantity: " + quantityRequest);
+                console.log("You have been charged: " + (order.quantityRequest * selectedItem[0].price) + " dollars.");
+                connection.query('UPDATE Bamazon.products SET stock_quantity=? WHERE id=?', [selectedItem[0].stock_quantity - quantityRequest, item_id],
+                    function(err, response) {
+                        if (err) throw err;
+                        startStore();
+                    });
+            } else {
+                console.log("Oops! We do not have enough of that item. Currently, we have " + selectedItem[0].stock_quantity + " " + selectedItem[0].product_name + " in stock. Please lower your quantity or check back another time.");
+            }
+        });
+
     });
 };
+
+startStore();
